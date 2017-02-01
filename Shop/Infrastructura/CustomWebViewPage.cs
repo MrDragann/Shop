4c20908bb57c5ae0bda5a6edb70d77c331f6f35f
MrDragan <MrDragan@MRDRAGAN-LP>
2017-02-02 01:15:18 +0200

    полностью перенес пример авторизации

---
+++ Shop/Infrastructura/CustomWebViewPage.cs
@@ -0,0 +1,39 @@
+﻿using IServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Shop.Infrastructura
+{
+    public class CustomWebViewPage : WebViewPage
+    {
+        public CustomWebViewPage()
+        {
+            User = WebUser.CurrentUser;
+        }
+
+        public new ModelUser User { get; set; }
+
+        public override void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CustomWebViewPage<T> : WebViewPage<T>
+    {
+        public CustomWebViewPage()
+        {
+            User = WebUser.CurrentUser;
+        }
+        public new ModelUser User { get; set; }
+
+        public override void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
